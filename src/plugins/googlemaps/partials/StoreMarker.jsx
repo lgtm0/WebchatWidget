f7d471a3f5d1391e63e0b70db3e45c838c9b4c76
NicoK <n.kruck@milestone-consult.de>
2025-07-23 16:10:14 +0200

    link change to blob

--- src/plugins/googlemaps/partials/StoreMarker.jsx
+++ src/plugins/googlemaps/partials/StoreMarker.jsx
@@ -35,7 +35,7 @@ const style = {
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center',
-  backgroundImage: `url(https://henkel-cognigy.s3.eu-central-1.amazonaws.com/plugins/googlemaps/map-marker.png)`,
+  backgroundImage: `url(https://sthkhcbgptstorage.blob.core.windows.net/public/plugins/googlemaps/map-marker.png)`,
   color: 'black',
   fontSize: 14,
   fontWeight: 600,
